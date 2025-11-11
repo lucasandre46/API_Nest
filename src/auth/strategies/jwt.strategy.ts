5683a1127de95ec0540f4ab2ad0c799268ef329e
lucasandre46 <lucasandremiguel46@gmail.com>
2025-11-10 21:38:06 -0300

    atlz_btn_toca_musica

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -11,6 +12,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
             secretOrKey: 'O_MEU_PROFESSOR_BRYAM_E_UM_BABACA',
         })
     }
+
+private static cookieExtractor(req: Request): string | null {
+    if(req.cookies && req.cookies.access_token) {
+        return req.cookies.access_token;
+    }
+    return null;
+}
+
 
     async validate(payload: any) {
         return { userId: payload.sub, email: payload.email, nome: payload.nome };
--- src/music/Model/musicBD.ts
+++ src/music/Model/musicBD.ts
@@ -9,7 +9,7 @@ export const Music_Data: MusicBD[] = [
     title: 'S.O.S.',
     description: 'Um dos clássicos de Raul Seixas, com crítica social e ritmo marcante.',
     imageUrl: 'https://tse3.mm.bing.net/th/id/OIP.lpp_ZNvTbiynwbeBZazU0wHaHa?rs=1&pid=ImgDetMain&o=7&rm=3',
-    audioUrl: '12dNSoF5a3mEQPpmRwY77CuUcp9KQNYCm',    
+    audioUrl: 'https://www.youtube.com/watch?v=kVheuv70Tv4',    
 
   },
   {
@@ -17,27 +17,27 @@ export const Music_Data: MusicBD[] = [
     title: 'Ska',
     description: 'Faixa vibrante do álbum *O Passo de Lui*, dos Paralamas do Sucesso.',
     imageUrl: 'https://tse4.mm.bing.net/th/id/OIP.-u70aF7GGeHyKuNn7tkfHgHaEK?rs=1&pid=ImgDetMain&o=7&rm=3',
-    audioUrl: 'https://example.com/audio/ska_paralamas.mp3',
+    audioUrl: 'https://youtu.be/Ktj_yLukC4I?si=C0J5_EaPO3yxmlpw',
   },
   {
     id: 3,
     title: 'Romance Ideal',
     description: 'Canção icônica dos Paralamas do Sucesso, misturando pop e ska brasileiro.',
     imageUrl: 'https://tse4.mm.bing.net/th/id/OIP.-u70aF7GGeHyKuNn7tkfHgHaEK?rs=1&pid=ImgDetMain&o=7&rm=3',
-    audioUrl: 'https://example.com/audio/romance_ideal_paralamas.mp3',
+    audioUrl: 'https://youtu.be/UYur6gZboKA?si=7PBOrbzTfWpFh7J-',
   },
   {
     id: 4,
     title: 'Vital e Sua Moto',
     description: 'O primeiro grande sucesso dos Paralamas, faixa de estreia do álbum *Cinema Mudo*.',
     imageUrl: 'https://images.coveralia.com/audio/o/Os_Paralamas_Do_Sucesso-Cinema_Mudo-Frontal.jpg',
-    audioUrl: 'https://example.com/audio/vital_e_sua_moto.mp3',
+    audioUrl: 'https://youtu.be/hiPfHRV8D4Y?si=hKSj3x_0Nnp7jY5d',
   },
   {
     id: 5,
     title: 'Garoto Maravilhoso',
     description: 'Raul Seixas em uma de suas músicas mais provocantes e bem-humoradas.',
     imageUrl: 'https://tse3.mm.bing.net/th/id/OIP.lpp_ZNvTbiynwbeBZazU0wHaHa?rs=1&pid=ImgDetMain&o=7&rm=3',
-    audioUrl: 'https://example.com/audio/garoto_maravilhoso.mp3',
+    audioUrl: 'https://youtu.be/PqR2boSe3-Q?si=oZlLAclsAK9yb5hj',
   },
 ];
